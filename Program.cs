7f1cc915b9409852a2401f33444a16acb2cb25a4
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-07 16:42:22 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiGia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap tien usd: ");
+            double usd=double.Parse(Console.ReadLine());
+            Console.WriteLine("So tien sau khi doi: " + usd * 23000 + " VND");
+            Console.ReadKey();
+        }
+    }
+}
